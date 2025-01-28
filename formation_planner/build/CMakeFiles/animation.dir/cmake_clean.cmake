d9e189b1f90872d423226fd7aae8450f4b4c0420
weijian <simonzhang1999@163.com>
2025-01-28 11:49:52 +0000

    initial commit

---
+++ formation_planner/build/CMakeFiles/animation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/animation.dir/src/formation_generation/animation.cpp.o"
+  "devel/lib/formation_planner/animation"
+  "devel/lib/formation_planner/animation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/animation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
